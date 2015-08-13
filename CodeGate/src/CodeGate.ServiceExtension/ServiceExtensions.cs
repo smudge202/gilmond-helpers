9a611fe7e61c4d88bf9db9e0572b1cdd17713e3c
Tommy Long <pete_201@hotmail.com>
2015-08-13 14:44:49 +0100

    Item Template with NuGet packages added

---
+++ CodeGate/src/CodeGate.ServiceExtension/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Framework.DependencyInjection;
+using System.Collections.Generic;
+
+namespace $rootnamespace$
+{
+	static class $safeitemrootname$
+	{
+		public static IServiceCollection AddServices(this IServiceCollection services)
+		{
+			services.TryAdd(GetDefaultServices());
+			return services;
+		}
+
+		public static IEnumerable<ServiceDescriptor> GetDefaultServices()
+		{
+			//yield return ServiceDescriptor.Transient<Abstraction, Implementation>();
+			yield break;
+		}
+	}
+}
