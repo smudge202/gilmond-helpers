89f9d90d4236f3459012e3b1e838d03bfa7433ae
Tommy Long <pete_201@hotmail.com>
2015-08-26 16:44:00 +0100

    Added event sourcing example structure

---
+++ Examples/EventSourcing/src/Cli/Program.cs
@@ -0,0 +1,16 @@
+﻿using Compose;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Cli
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.UseServices(services =>
+                services.AddTransient<object>()
+            );
+        }
+    }
+}
---
+++ Examples/EventSourcing/tests/Purchasing.Tests/CompositionSpecs.cs
@@ -0,0 +1,13 @@
+﻿using TestAttributes;
+
+namespace Purchasing.Tests
+{
+    public class CompositionSpecs
+    {
+        [Unit]
+        public void ProvidesService()
+        {
+
+        }
+    }
+}
